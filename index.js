5013e1f1cb4c722596ebf21844a13849c26a7bd9
Slynax <remlouplay3@gmail.com>
2025-01-30 13:37:54 +0100

    fix pv

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import amqp from 'amqplib';
-// import { generateMap } from './helpers/generateMap.js';
 
 const app = express();
 
